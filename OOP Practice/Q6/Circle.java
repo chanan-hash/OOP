35ad6dc82952658a8088e5355a1b67a26d68177e
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2023-12-25 10:58:18 +0200

    Adding intersection function

--- OOP Practice/Q6/Circle.java
+++ OOP Practice/Q6/Circle.java
@@ -35,4 +35,49 @@ public class Circle {
     public double perimeter() {
         return (2 * PI * this.radius);
     }
+
+    /**
+     * This is not a static function, because it depends on creating an instance of the class
+     *
+     * @param c
+     * @return The distance between 'this' and a given circle
+     */
+    public double dist1(Circle c) {
+        return this.center.distance(c.center);
+    }
+
+    /**
+     * This is a static function, because it is not depending on creating an instance of the class
+     *
+     * @param c1
+     * @param c2
+     * @return dist --> the distance between both centers
+     */
+    public static double dist2(Circle c1, Circle c2) {
+        return c1.center.distance(c2.center);
+    }
+
+    // Based on geeksforgeeks. This is a static function from the same reason above
+    // https://www.geeksforgeeks.org/area-of-intersection-of-two-circles/
+    public static double intersectionArea(Circle c1, Circle c2) {
+        double area = 0;
+        double dist, alpha, beta, a1, a2;
+        dist = c1.center.distance(c2.center);
+        if (dist > c1.radius + c2.radius) { // means the two circles are to far from each other
+            return 0;
+        } else if (dist <= (c1.radius - c2.radius) && c1.radius >= c2.radius) {
+            area = (PI * c2.radius * c2.radius); // PI radius^2
+        } else if (dist <= (c2.radius - c1.radius) && c2.radius >= c1.radius) {
+            area = (PI * Math.pow(c1.radius, 2));
+        } else {
+            alpha = Math.acos((c1.radius * c1.radius + dist * dist - c2.radius * c2.radius) / (2 * c1.radius * dist)) * 2;
+            beta = Math.acos((c2.radius * c2.radius + dist * dist - c1.radius * c1.radius) / (2 * c2.radius * dist)) * 2;
+            a1 = 0.5 * beta * c2.radius * c2.radius - 0.5 * c2.radius * c2.radius * Math.sin(beta);
+            a2 = 0.5 * alpha * c1.radius * c1.radius - 0.5 * c1.radius * c1.radius * Math.sin(alpha);
+            area = (a1 + a2);
+        }
+        return area;
+    }
+
+
 }
