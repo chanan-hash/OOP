377493098e3609700b604dfc1f5506dad2b90083
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-01-07 19:00:38 +0200

    Starting the board

--- Assignment_1_OOP/src/GameLogic.java
+++ Assignment_1_OOP/src/GameLogic.java
@@ -1,4 +1,34 @@
-public class GameLogic implements PlayableLogic{
+public class GameLogic implements PlayableLogic {
+    /**
+     * This class represents the game logic of the game. and implements the PlayableLogic interface.
+     * here we'll have the running game methods.
+     */
+
+    private final ConcretePiece[][] board; // The board of the game 2D array of ConcretePiece
+    private final ConcretePiece[] peices; // The pieces of the game from 0-12 players 1 and 13-36 players 2
+
+
+    private final ConcretePlayer player1;
+    private final ConcretePlayer player2;
+
+    private boolean isPlayer2Turn; // To know which player turn is it
+
+    private Position king1Position; // The position of the king of player 1
+
+
+    // Constructor
+    public GameLogic(){
+        board = new ConcretePiece[11][11]; // Initializing the board
+        peices = new ConcretePiece[37]; // Initializing the pieces
+        player1 = new ConcretePlayer(true); // Initializing the players
+        player2 = new ConcretePlayer(false);
+
+        isPlayer2Turn = true; // Initializing the turn to player 2
+//        player1Pieces();
+//        player2Pieces();
+
+    }
+
     @Override
     public boolean move(Position a, Position b) {
         return false;
@@ -6,17 +36,17 @@ public class GameLogic implements PlayableLogic{
 
     @Override
     public Piece getPieceAtPosition(Position position) {
-        return null;
+        return board[position.getY()][position.getX()];
     }
 
     @Override
     public Player getFirstPlayer() {
-        return null;
+        return this.player1;
     }
 
     @Override
     public Player getSecondPlayer() {
-        return null;
+        return this.player2;
     }
 
     @Override
@@ -26,7 +56,7 @@ public class GameLogic implements PlayableLogic{
 
     @Override
     public boolean isSecondPlayerTurn() {
-        return false;
+        return isPlayer2Turn;
     }
 
     @Override
