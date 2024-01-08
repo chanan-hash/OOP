dbd432068cb26b7529ca6207a19878cf4fd7994e
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-01-08 16:30:54 +0200

    working on moves function

--- Assignment_1_OOP/src/GameLogic.java
+++ Assignment_1_OOP/src/GameLogic.java
@@ -17,7 +17,7 @@ public class GameLogic implements PlayableLogic {
 
 
     // Constructor
-    public GameLogic(){
+    public GameLogic() {
         board = new ConcretePiece[11][11]; // Initializing the board
         pieces = new ConcretePiece[37]; // Initializing the pieces
         player1 = new ConcretePlayer(true); // Initializing the players
@@ -29,15 +29,15 @@ public class GameLogic implements PlayableLogic {
 
     }
 
-    public void player1Pieces(){
+    public void player1Pieces() {
         // Creating the pieces of player 1 starting with the king
-        King king = new King(player1,"♔",7);
+        King king = new King(player1, "♔", 7);
         pieces[6] = king;
         for (int i = 0; i < 13; i++) {
-            if (i == 6){
+            if (i == 6) {
                 continue;
             }
-            pieces[i] = new Pawn(player1,"♙",i + 1);
+            pieces[i] = new Pawn(player1, "♙", i + 1);
         }
 
         // Putting the pieces on the board.
@@ -47,55 +47,55 @@ public class GameLogic implements PlayableLogic {
 
         // Row 3
         board[3][5] = pieces[0];
-        pieces[0].addMove(new Position(5,3)); // For printing we're including also the first move
+        pieces[0].addMove(new Position(5, 3)); // For printing we're including also the first move
 
         // Row 4
         board[4][4] = pieces[1];
-        pieces[1].addMove(new Position(4,4));
+        pieces[1].addMove(new Position(4, 4));
 
         board[4][5] = pieces[2];
-        pieces[2].addMove(new Position(5,4));
+        pieces[2].addMove(new Position(5, 4));
 
         board[4][6] = pieces[3];
-        pieces[3].addMove(new Position(6,4));
+        pieces[3].addMove(new Position(6, 4));
 
         // Row 5
         board[5][3] = pieces[4];
-        pieces[4].addMove(new Position(3,5));
+        pieces[4].addMove(new Position(3, 5));
 
         board[5][4] = pieces[5];
-        pieces[5].addMove(new Position(4,5));
+        pieces[5].addMove(new Position(4, 5));
 
         // The king
         board[5][5] = pieces[6];
-        pieces[6].addMove(new Position(5,5));
+        pieces[6].addMove(new Position(5, 5));
 
         board[5][6] = pieces[7];
-        pieces[7].addMove(new Position(6,5));
+        pieces[7].addMove(new Position(6, 5));
 
         board[5][7] = pieces[8];
-        pieces[8].addMove(new Position(7,5));
+        pieces[8].addMove(new Position(7, 5));
 
         // Row 6
         board[6][4] = pieces[9];
-        pieces[9].addMove(new Position(4,6));
+        pieces[9].addMove(new Position(4, 6));
 
         board[6][5] = pieces[10];
-        pieces[10].addMove(new Position(5,6));
+        pieces[10].addMove(new Position(5, 6));
 
         board[6][6] = pieces[11];
-        pieces[11].addMove(new Position(6,6));
+        pieces[11].addMove(new Position(6, 6));
 
         // Row 7
         board[7][5] = pieces[12];
-        pieces[12].addMove(new Position(5,7));
+        pieces[12].addMove(new Position(5, 7));
 
     }
 
-    public void player2Pieces(){
+    public void player2Pieces() {
         // Creating the pieces of player 2, he has only pawns
         for (int i = 13; i < 37; i++) {
-            pieces[i] = new Pawn(player2,"♟",i - 12);
+            pieces[i] = new Pawn(player2, "♟", i - 12);
         }
 
         // Putting the pieces on the board.
@@ -103,90 +103,147 @@ public class GameLogic implements PlayableLogic {
 
         // Row 0
         board[0][3] = pieces[13];
-        pieces[13].addMove(new Position(3,0));
+        pieces[13].addMove(new Position(3, 0));
 
         board[0][4] = pieces[14];
-        pieces[14].addMove(new Position(4,0));
+        pieces[14].addMove(new Position(4, 0));
 
         board[0][5] = pieces[15];
-        pieces[15].addMove(new Position(5,0));
+        pieces[15].addMove(new Position(5, 0));
 
         board[0][6] = pieces[16];
-        pieces[16].addMove(new Position(6,0));
+        pieces[16].addMove(new Position(6, 0));
 
         board[0][7] = pieces[17];
-        pieces[17].addMove(new Position(7,0));
+        pieces[17].addMove(new Position(7, 0));
 
         // Row 1
         board[1][5] = pieces[18];
-        pieces[18].addMove(new Position(5,1));
+        pieces[18].addMove(new Position(5, 1));
 
         // Row 3
         board[3][0] = pieces[19];
-        pieces[19].addMove(new Position(0,3));
+        pieces[19].addMove(new Position(0, 3));
 
         board[3][10] = pieces[20];
-        pieces[20].addMove(new Position(10,3));
+        pieces[20].addMove(new Position(10, 3));
 
         // Row 4
         board[4][0] = pieces[21];
-        pieces[21].addMove(new Position(0,4));
+        pieces[21].addMove(new Position(0, 4));
 
         board[4][10] = pieces[22];
-        pieces[22].addMove(new Position(10,4));
+        pieces[22].addMove(new Position(10, 4));
 
         // Row 5
         board[5][0] = pieces[23];
-        pieces[23].addMove(new Position(0,5));
+        pieces[23].addMove(new Position(0, 5));
 
         board[5][1] = pieces[24];
-        pieces[24].addMove(new Position(1,5));
+        pieces[24].addMove(new Position(1, 5));
 
         board[5][9] = pieces[25];
-        pieces[25].addMove(new Position(9,5));
+        pieces[25].addMove(new Position(9, 5));
 
         board[5][10] = pieces[26];
-        pieces[26].addMove(new Position(10,5));
+        pieces[26].addMove(new Position(10, 5));
 
         // Row 6
         board[6][0] = pieces[27];
-        pieces[27].addMove(new Position(0,6));
+        pieces[27].addMove(new Position(0, 6));
 
         board[6][10] = pieces[28];
-        pieces[28].addMove(new Position(10,6));
+        pieces[28].addMove(new Position(10, 6));
 
         // Row 7
         board[7][0] = pieces[29];
-        pieces[29].addMove(new Position(0,7));
+        pieces[29].addMove(new Position(0, 7));
 
         board[7][10] = pieces[30];
-        pieces[30].addMove(new Position(10,7));
+        pieces[30].addMove(new Position(10, 7));
 
         // Row 9
         board[9][5] = pieces[31];
-        pieces[31].addMove(new Position(5,9));
+        pieces[31].addMove(new Position(5, 9));
 
         // Row 10
         board[10][3] = pieces[32];
-        pieces[32].addMove(new Position(3,10));
+        pieces[32].addMove(new Position(3, 10));
 
         board[10][4] = pieces[33];
-        pieces[33].addMove(new Position(4,10));
+        pieces[33].addMove(new Position(4, 10));
 
         board[10][5] = pieces[34];
-        pieces[34].addMove(new Position(5,10));
+        pieces[34].addMove(new Position(5, 10));
 
         board[10][6] = pieces[35];
-        pieces[35].addMove(new Position(6,10));
+        pieces[35].addMove(new Position(6, 10));
 
         board[10][7] = pieces[36];
-        pieces[36].addMove(new Position(7,10));
+        pieces[36].addMove(new Position(7, 10));
 
     }
 
 
     @Override
     public boolean move(Position a, Position b) {
+        // First we will check which in this position
+        ConcretePiece currPiece = board[a.getY()][a.getX()]; // Getting the current piece in that position
+        Player currPlayer = currPiece.getOwner();
+//        Player currPlayer = isPlayer2Turn ? player2 : player1; // Getting the current player
+
+        // checking first the position correction
+        if (currPiece == null) { // if it is an empty position
+            return false;
+        }
+
+        if (a.equals(b)){
+            return false; // This is the same place
+        }
+
+        // if(!(a.getX() == b.getX() || a.getY() == b.getY())
+        if (a.getX() != b.getX() || a.getY() != b.getY()) { // Checking if he tries to move diagonal
+            return false;
+        }
+
+        // Checking if the position is a corner and the piece isn't a king
+        if(b.getX() == 0 && b.getY() == 0 || b.getX() == 0 && b.getY() == 10 || b.getX() == 10 && b.getY() == 0 || b.getX() == 10 && b.getY() == 10 && !(currPiece instanceof King)){
+            return false;
+        }
+
+        // Now we need to check if it is a pawn or a king
+        // if(p instanceof King){ // If it is a king
+        if (King.class.isInstance(currPiece)) {
+
+
+        } else { // It's a pawn
+
+        }
+        return false;
+    }
+
+    private void eatCheck(){ // Check if can it
+
+    }
+
+    public boolean isFinished() {
+        // Checking if the king got to one of the corners
+        //  if (board[0][0] instanceof King || board[0][10] instanceof King || board[10][0] instanceof King || board[10][10] instanceof King) {
+        if(king1Position == new Position(0, 0) || king1Position == new Position(0, 10) || king1Position == new Position(10, 0) || king1Position == new Position(10, 10)){
+            player1.addWin();
+            // Printing the data
+            return true;
+        }
+
+        // Checking 3 directions
+
+        // Checking if the king is surrounded by player's 2 pawns
+        // 4 directions
+        else if(board[king1Position.getY() + 1][king1Position.getX()].getOwner() == player2 && board[king1Position.getY() - 1][king1Position.getX()].getOwner() == player2 && board[king1Position.getY()][king1Position.getX() + 1].getOwner() == player2 && board[king1Position.getY()][king1Position.getX() - 1].getOwner() == player2) {
+            player2.addWin();
+            // Printing the data
+            return true;
+        }
         return false;
     }
 
