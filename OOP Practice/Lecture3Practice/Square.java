87b1af67acb257c5fa65d55b83820fcb70323fa6
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-01-22 13:07:38 +0200

    Updating square

--- OOP Practice/Lecture3Practice/Square.java
+++ OOP Practice/Lecture3Practice/Square.java
@@ -1,24 +1,27 @@
 package Lecture3Practice;
 
 public class Square extends GeometricShape{
-    private double _x1, _x2; // Needs to points for size of square, all the sides are equal
+    private double _x1, _y1,_x2, _y2; // The Square is defined by 2 points
 
-    public Square(double x1, double y1, double x2) {
+    public Square(double x1, double y1, double x2,double y2) {
         this._x1 = x1;
+        this._y1 = y1;
         this._x2 = x2;
+        this._y2 = y2;
+    }
+    private double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
     }
     @Override
-//    public double getArea() {
-//        return Math.abs(_x2 - _x1) * Math.abs(_y2 - _y1); //
-//    }
-//
     public double getArea() {
-        return Math.pow(_x2 - _x1,2); // because this is a square
+        double side = distance(_x1, _y1, _x2, _y2);
+        return Math.pow(side,2); // because this is a square
     }
 
     @Override
     public double getPerimeter() {
-        return 4 * Math.abs(_x2 - _x1);
+        double side = distance(_x1, _y1, _x2, _y2);
+        return 4 * side;
     }
 
     @Override
