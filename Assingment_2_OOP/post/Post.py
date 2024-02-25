251ee77ef1de8fa7e60689c9713e069216bdfa58
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-02-25 18:34:13 +0200

    Working on Post

--- Assingment_2_OOP/post/Post.py
+++ Assingment_2_OOP/post/Post.py
@@ -10,4 +10,14 @@ from abc import ABC, abstractmethod
 from CostomException import NotLoginError
 from Notificator import Notificator
 
+
 class Post(ABC):
+    _notificator = Notificator()
+
+    def __init__(self, creator: User):
+        self._creator = creator
+        self._likes: Set = set()  # Creating a set that will hold the likes on the post. each user can do one like for a post
+
+        Post._notificator.notify_all_followers(creator,f"{creator.username} has a new post") # Notifying on a post
+
+    def like (self, user: User):
