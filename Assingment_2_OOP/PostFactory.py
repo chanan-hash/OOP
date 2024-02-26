8fc07c4ca9362c016367778c8fd9ba51cdcea7c4
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-02-26 09:42:24 +0200

    Adding Factory pattern for posts

--- Assingment_2_OOP/PostFactory.py
+++ Assingment_2_OOP/PostFactory.py
@@ -1,1 +1,22 @@
-pass
+import args
+
+from post.ImagePost import ImagePost
+from post.TesxtPost import TextPost
+from post.SalePost import SalePost
+from post.Post import Post
+
+
+class PostFactory:
+    @staticmethod
+    def create_post(post_type: str, creator, *args) -> Post:
+        if post_type == "Text":
+            post = TextPost(creator, *args)
+        elif post_type == "Sale":
+            post = SalePost(creator, *args)
+        elif post_type == "Image":
+            post = ImagePost(creator, *args)
+        else:
+            raise ValueError(f"Invalid post type for type: {post_type}")
+
+        print(post)
+        return post
