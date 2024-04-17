798a37d0f4a7f1cf1affc5f5270cc1d10ea4e10b
DESKTOP-JK2VDE2\חנן <chananhelman@gmail.com>
2024-04-17 15:42:36 +0300

    creating persons to the flights

--- Final assignment/Ex1A/FlightCompany.java
+++ Final assignment/Ex1A/FlightCompany.java
@@ -10,28 +10,40 @@ public class FlightCompany {
 
     private final List<FlightCompany> subCompanies;
     private final List<Flight> flights;
-    private final Map<Flight, ArrayList<FilghtObserver>> flightObservers;
+//    private final Map<Flight, ArrayList<FilghtObserver>> flightObservers;
 
 
     public FlightCompany(String companyName){
         this.subCompanies = new ArrayList<>();
         this.flights = new ArrayList<>();
-        this.flightObservers = new HashMap<>();
+//        this.flightObservers = new HashMap<>();
     }
-    /*
-    @Override
-    public boolean bookFlight(Flight flight, boolean subscribe){
+
+    public boolean bookFlight(Flight flight, Passengers passengers ,boolean subscribe){
         if(flight.getPassengers().size() < flight.getNumPassengers()){
-            flight.getPassengers().add(this);
+            flight.getPassengers().add(passengers);
             if (subscribe){
-                flight.addObserver(this);
+                flight.addObserver((FilghtObserver) passengers); // TODO check the casting
             }
             return true;
         }
         return false;
     }
-     */
 
+    // TODO Notify the observer
+    public boolean cancelFlight(Flight flight, Passengers passengers){
+        if(flight.getPassengers().contains(passengers)){
+            flight.getPassengers().remove(passengers);
+            flight.removeObserver((FilghtObserver) passengers);
+            // Notify the observer
+//            flightObservers.get(flight).remove(passengers);
+            flight.notifyPassenger(); //TODO add message of canceling
+            return true;
+        }
+        return false;
+    }
+
+    // TODO add flight function
         /*
     printData(){
         print the data of the curr company
